838c10566f3f302348ea7e8c6ecbd877eedfaf13
John Wesley <kbjwes77@gmail.com>
2018-08-18 22:00:13 -0400

    test4

--- js/loadfeed.js
+++ js/loadfeed.js
@@ -1,13 +1,13 @@
-function loadfeed(feed_min,feed_max,feed_len,fulltext)
+function loadfeed(feed_min,feed_max,feed_len,shorten)
 	{
-	var req;
 	for (var i=0; i<feed_len; i++)
 		{
-		(function(feed_ind,post_div,readmore) // need function closure
+		(function(feed_ind,post_div) // need function closure
 			{
 			if (feed_ind >= feed_min && feed_ind <= feed_max)
 				{
-				req = new XMLHttpRequest();
+				var req = new XMLHttpRequest();
+				req.readmore = shorten;
 				req.open("GET","/feed/post" + feed_ind + "/post.html", true);
 				req.onreadystatechange = function()
 					{
@@ -15,7 +15,7 @@ function loadfeed(feed_min,feed_max,feed_len,fulltext)
 					if (this.status!==200) return;
 					document.getElementById("post-" + post_div).innerHTML = this.responseText;
 					
-					if (readmore) // !fulltext
+					if (this.readmore) // !fulltext
 						{
 						$clamp(document.getElementById("post-" + feed_ind + "-text"), {clamp: 3});
 						}
@@ -27,6 +27,6 @@ function loadfeed(feed_min,feed_max,feed_len,fulltext)
 					};
 				req.send();
 				};
-			})(feed_min+i,i+1,!fulltext) // closure so variables aren't changed inside the loop from outside (stupid ASYNC)
+			})(feed_min+i,i+1) // closure so variables aren't changed inside the loop from outside (stupid ASYNC)
 		};
 	};
