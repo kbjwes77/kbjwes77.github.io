5df5a390796501492205534984b9eef3d07860ea
John Wesley <kbjwes77@gmail.com>
2018-08-18 21:28:53 -0400

    some closure fixes

--- js/loadfeed.js
+++ js/loadfeed.js
@@ -1,25 +1,25 @@
-function loadfeed(feed_min,feed_max,feed_len,clamp)
+function loadfeed(feed_min,feed_max,feed_len,fulltext)
 	{
 	var req;
 	for (var i=0; i<feed_len; i++)
 		{
-		(function(feed_ind,post_div) // need function closure
+		(function(feed_ind,post_div,readmore) // need function closure
 			{
 			if (feed_ind >= feed_min && feed_ind <= feed_max)
 				{
 				req = new XMLHttpRequest();
 				req.open("GET","/feed/post" + feed_ind + "/post.html", true);
-				req.onreadystatechange= function()
+				req.onreadystatechange = function()
 					{
 					if (this.readyState!==4) return;
 					if (this.status!==200) return;
 					document.getElementById("post-" + post_div).innerHTML = this.responseText;
 					
-					if (clamp)
+					if (readmore) // !fulltext
 						{
-						$clamp(document.getElementById("post-" + post_div + "-text"), {clamp: 3});
+						$clamp(document.getElementById("post-" + feed_ind + "-text"), {clamp: 3});
 						}
-					else
+					else // fulltext
 						{
 						var element = document.getElementById("read-more-" + feed_ind);
 						element.parentNode.removeChild(element);
@@ -27,6 +27,6 @@ function loadfeed(feed_min,feed_max,feed_len,clamp)
 					};
 				req.send();
 				};
-			})(feed_min+i,i+1) // closure so variables aren't changed inside the loop from outside (stupid ASYNC)
+			})(feed_min+i,i+1,!fulltext) // closure so variables aren't changed inside the loop from outside (stupid ASYNC)
 		};
 	};
