846827f9fe8cd1ade0007e6316150dba08b5ee53
John Wesley <kbjwes77@gmail.com>
2018-08-21 00:51:39 -0400

    buncha stuff

--- js/kb.js
+++ js/kb.js
@@ -1,3 +1,20 @@
+// query URL for fields
+function urlQuery(field)
+	{
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++)
+		{
+		var pair = vars[i].split("=");
+		if(pair[0] == field)
+			{
+			return(pair[1]);
+			};
+		};
+	return("");
+	};
+
+// load feed
 function loadfeed(feed_min,feed_max,feed_len,shorten)
 	{
 	for (var i=0; i<feed_len; i++)
@@ -29,4 +46,18 @@ function loadfeed(feed_min,feed_max,feed_len,shorten)
 				};
 			})(feed_min+i,i+1) // closure so variables aren't changed inside the loop from outside (stupid ASYNC)
 		};
+	};
+
+// load a template into an element by ID
+function loadtemp(file,element)
+	{
+	var req = new XMLHttpRequest();
+	req.open("GET",file,true);
+	req.onreadystatechange = function()
+		{
+		if (this.readyState!==4) return;
+		if (this.status!==200) return;
+		document.getElementById(element).innerHTML = this.responseText;
+		};
+	req.send();
 	};
