26c11ec81f52e33c6dcbcec6d7b433f3fc7d923f
kbjwes77 <kbjwes77@gmail.com>
2020-01-25 23:03:35 -0500

    Removed Feed, Created Header Template

--- js/kb.js
+++ js/kb.js
@@ -14,43 +14,27 @@ function urlQuery(field)
 	return("");
 	};
 
-// load feed
-function loadfeed(feed_min,feed_max,feed_len,shorten)
-	{
-	for (var i=0; i<feed_len; i++)
-		{
-		(function(feed_ind,post_div) // need function closure
-			{
-			if (feed_ind >= feed_min && feed_ind <= feed_max)
-				{
-				var req = new XMLHttpRequest();
-				req.readmore = shorten;
-				req.open("GET","/feed/post" + feed_ind + "/post.html", true);
-				req.onreadystatechange = function()
-					{
-					if (this.readyState!==4) return;
-					if (this.status!==200) return;
-					document.getElementById("post-" + post_div).innerHTML = this.responseText;
-					
-					if (this.readmore) // !fulltext
-						{
-						$clamp(document.getElementById("post-" + feed_ind + "-text"), {clamp: 3});
-						}
-					else // fulltext
-						{
-						var element = document.getElementById("read-more-" + feed_ind);
-						element.parentNode.removeChild(element);
-						};
-					};
-				req.send();
-				};
-			})(feed_min+i,i+1) // closure so variables aren't changed inside the loop from outside (stupid ASYNC)
-		};
-	};
-
 // load a template into an element by ID
-function loadtemp(file,element)
+function loadTemplate(file,element)
 	{
+	if (element == "header")
+		{
+		switch(pageType)
+			{
+			case "home":
+				file = "/templates/header-home.html";
+				break;
+			case "game":
+				file = "/templates/header-game.html";
+				break;
+			case "music":
+				file = "/templates/header-music.html";
+				break;
+			case "contact":
+				file = "/templates/header-contact.html";
+				break;
+			}
+		}
 	var req = new XMLHttpRequest();
 	req.open("GET",file,true);
 	req.onreadystatechange = function()
