5cc7d2a29564629a12c48453ca9d783d943c0b5c
kbjwes77 <kbjwes77@gmail.com>
2020-11-10 23:50:46 -0500

    Init Tooltips

--- js/common.js
+++ js/common.js
@@ -56,4 +56,7 @@ const load_template = async function(file,element)
 window.onload = () => {
 	set_header();
 	set_footer();
+	
+	// init tooltips (requires jquery, popper, bootstrap)
+	$('[data-toggle="tooltip"]').tooltip();
 	}
